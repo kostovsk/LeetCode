b26d3b2a91dae668a9e21339faebcef174d3f182
Stoycho Kostov <stoichokostov@gmail.com>
2021-03-30 17:27:45 -0500

    create_proj

---
+++ Lucky Numbers in a Matrix v1.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lucky_Numbers_in_a_Matrix_v1._0
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
