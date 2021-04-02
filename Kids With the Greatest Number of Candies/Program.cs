f652471bc3cb719dceade96362ad56613c2e4ae1
Stoycho Kostov <stoichokostov@gmail.com>
2021-04-02 06:51:53 -0500

    kids_proj

---
+++ Kids With the Greatest Number of Candies/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kids_With_the_Greatest_Number_of_Candies
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
