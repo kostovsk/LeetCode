72d12eb1be918055e5561ea1547932d6c26b5cef
Stoycho Kostov <stoichokostov@gmail.com>
2021-04-02 06:57:21 -0500

    working_proj

--- Kids With the Greatest Number of Candies/Program.cs
+++ Kids With the Greatest Number of Candies/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Kids_With_the_Greatest_Number_of_Candies
 {
    class Program
    {
+      public static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+      {
+         List<bool> result = new List<bool>();
+         int a = extraCandies;
+         int maxVal = 5;
+
+         //find maxVal from array
+         int max = candies[0];
+         for (int k = 1; k < candies.Length; k++)
+         {
+            if (candies[k] > max)
+            {
+               max = candies[k];
+            }
+         }
+
+         for (int i = 0; i < candies.Length; i++)
+         {
+            if (candies[i] + a >= max)
+            {
+               result.Add(true);
+            }
+            else
+            {
+               result.Add(false);
+            }
+
+         }
+         return result;
+      }
       static void Main(string[] args)
       {
-         Console.WriteLine("Hello World!");
+         int[] numC = new int[] { 2, 3, 5, 1, 3 };
+         var extC = 5;
+         var result = KidsWithCandies(numC, extC);
+
+         foreach (var item in result)
+         {
+            Console.WriteLine(item);
+         }
       }
    }
 }
