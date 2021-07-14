ba22922e53782315684f7660c6401b6e07d0b99a
Stoycho Kostov <stoichokostov@gmail.com>
2021-07-13 20:54:50 -0500

    two sum

---
+++ Two Sum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Two_Sum
+{
+   class Program
+   {
+      public static int[] TwoSum(int[] nums, int target)
+      {
+         int[] result = new int[2];
+
+         for (int i = 0; i < nums.Length; i++)
+         {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+               if (nums[i] + nums[j] == target)
+               {
+                  result[0] = i;
+                  result[1] = j;
+                  break;
+               }
+
+            }
+         }
+
+         return result;
+      }
+
+      static void Main(string[] args)
+      {
+         int target = 6;
+         int[] nums = new int[] { 2, 4, 3 };
+
+         Console.WriteLine(TwoSum(nums, target));
+      }
+   }
+}
