3e679c6642087361ffa7041610c437d5825bbb62
Stoycho Kostov <stoichokostov@gmail.com>
2021-07-22 22:24:28 -0500

    climb stairs

---
+++ Climbing Stairs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Climbing_Stairs
+{
+   class Program
+   {
+      public static int fib(int f)
+      {
+         if (f <= 1)
+            return f;
+         else
+            return fib(f - 1) + fib(f - 2);
+      }
+
+      public static int ClimbStairs(int n)
+      {
+         int result = 0;
+
+         if (n >= 1 && n <= 45)
+            result = fib(n + 1);
+
+         return result;
+      }
+
+      static void Main(string[] args)
+      {
+         int num = 5;
+         Console.WriteLine(ClimbStairs(num));
+      }
+   }
+}
---
+++ Maximum Subarray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Maximum_Subarray
+{
+   class Program
+   {
+      public static int maxSubArraySum(int[] a)
+      {
+         int max = int.MinValue;
+         int max_end = 0;
+
+         for (int i = 0; i < a.Length; i++)
+         {
+            max_end = max_end + a[i];
+
+            if (max < max_end)
+               max = max_end;
+
+            if (max_end < 0)
+               max_end = 0;
+         }
+
+         return max;
+      }
+
+      static void Main(string[] args)
+      {
+         int[] a = { -2, -3, 4, -1, -2, 1, 5, -3 };
+         Console.Write("Maximum contiguous sum is " +
+                                 maxSubArraySum(a));
+      }
+   }
+}
