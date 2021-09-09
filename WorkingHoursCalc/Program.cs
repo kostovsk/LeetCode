ca963866f0a76eb8f24d012453d4f762ed1d587e
Stoycho Kostov <stoichokostov@gmail.com>
2021-09-09 17:54:44 -0500

    test

---
+++ WorkingHoursCalc/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace WorkingHoursCalc
+{
+   class Program
+   {
+      public static string CalculateTAT(DateTime? receive, DateTime? reply)
+      {
+         DateTime[] bankHolidays = new DateTime[]
+         {
+                new DateTime(2021, 1, 01),
+                new DateTime(2021, 1, 18),
+                new DateTime(2021, 2, 15),
+                new DateTime(2021, 5, 31),
+                new DateTime(2021, 6, 19),
+                new DateTime(2021, 7, 04),
+                new DateTime(2021, 9, 06),
+                new DateTime(2021, 10, 11),
+                new DateTime(2021, 11, 11),
+                new DateTime(2021, 11, 25),
+                new DateTime(2021, 12, 25),
+         };
+
+         receive = receive ?? default(DateTime);
+         reply = reply ?? default(DateTime);
+
+         string result = "";
+
+         TimeSpan totalHours = TimeSpan.Zero;
+         TimeSpan fullDay = new TimeSpan(08, 00, 00);
+         TimeSpan start = new TimeSpan(08, 30, 00);
+         TimeSpan end = new TimeSpan(16, 30, 00);
+
+         if (receive.Value.Date == reply.Value.Date)
+         {
+            if (receive.Value.TimeOfDay < start)
+            {
+               totalHours = (reply.Value.TimeOfDay - start);
+               return ((int)totalHours.TotalHours + ":" + totalHours.Minutes.ToString("D2"));
+            }
+            else if (receive.Value.TimeOfDay >= start && reply.Value.TimeOfDay <= end)
+            {
+               totalHours = (reply.Value.TimeOfDay - receive.Value.TimeOfDay);
+               return ((int)totalHours.TotalHours + ":" + totalHours.Minutes.ToString("D2"));
+            }
+         }
+
+
+         for (DateTime date = receive.Value; date.Date <= reply.Value.Date; date = date.AddDays(1))
+         {
+            if ((int)date.DayOfWeek >= 1 && (int)date.DayOfWeek <= 5)
+            {
+               totalHours = totalHours + fullDay;
+            }
+         }
+
+         foreach (DateTime bankHoliday in bankHolidays)
+         {
+            DateTime bh = bankHoliday.Date;
+            if (receive <= bh && bh <= reply)
+            {
+               totalHours = totalHours - fullDay;
+            }
+         }
+
+         if (receive.Value.TimeOfDay < start)
+         {
+            totalHours = totalHours - (end - reply.Value.TimeOfDay);
+         }
+         else if (receive.Value.TimeOfDay > start && receive.Value.TimeOfDay < end)
+         {
+            totalHours = totalHours - (receive.Value.TimeOfDay - start) - (end - reply.Value.TimeOfDay);
+         }
+         else if (receive.Value.TimeOfDay > end)
+         {
+            totalHours = totalHours - fullDay - (end - reply.Value.TimeOfDay);
+         }
+
+         result = ((int)totalHours.TotalHours + ":" + totalHours.Minutes.ToString("D2"));
+
+         return result;
+      }
+      static void Main(string[] args)
+      {
+         DateTime receive = new DateTime(2021, 07, 20);
+         DateTime reply = new DateTime(2021, 07, 28);
+
+         string test = CalculateTAT(receive, reply);
+
+         Console.WriteLine(test);
+      }
+   }
+}
---
+++ test2/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+
+namespace test2
+{
+   class Program
+   {
+      public static string[] FindCommon(string[] names1, string[] names2)
+      {
+         var combo = names1.Union(names2);
+         
+         string[] result = combo.Select(p => p).ToArray();
+
+         return result;
+      }
+
+      public static int solution(int[] A)
+      {
+         // write your code in C# 6.0 with .NET 4.5 (Mono)
+         int result = 0;
+         int len = A.Length - 1;
+
+         for (int i = 0; i < len; i++)
+         {
+            if (A[i] == A[i + 1])
+               result++;
+         }
+
+         if (result == len)
+            return result - 1;
+
+         int max = 0;
+         
+         for (int j = 0; j < A.Length; j++)
+         {
+            int test = 0;
+            if(j > 0)
+            {
+               if (A[j - 1] != A[j])
+                  test++;
+               else
+                  test--;
+            }
+            if(j < len)
+            {
+               if (A[j] != A[j + 1])
+                  test++;
+               else
+                  test--;
+            }
+            max = Math.Max(max, test);
+         }
+
+         return result;
+      }
+
+      public static int solution2(string S)
+      {
+         int result = 0;
+         int len = S.Length - 1;
+
+         for (int i = 2; i < len;)
+         {
+            if (S[i] == 'X' || S[i - 1] == 'X' || S[i - 2] == 'X')
+            {
+               result += 1;
+            }
+            if (i + 3 > len)
+            {
+               for (int j = len; j > i; j--)
+               {
+                  if (S[j] == 'X')
+                  {
+                     result += 1;
+                  }
+               }
+            }
+            i = i + 3;
+         }
+
+         return result;
+      }
+
+      public static void Main(string[] args)
+      {
+         //string[] names1 = new string[] { "Ava", "Emma", "Olivia" };
+         //string[] names2 = new string[] { "Olivia", "Sophia", "Emma" };
+
+         //Console.WriteLine(FindCommon(names1, names2)); // should print Ava, Emma, Olivia, Sophia
+
+         int[] test1 = new int[] { 1, 1, 0, 1, 1 };
+         Console.WriteLine(solution(test1));
+
+         //string input = ".X..X";
+         //Console.WriteLine(solution2(input));
+      }
+   }
+}
